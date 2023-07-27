a637b37d5d949de47149bd8a1016071bf7571dde
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-27 13:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+function startDragging(event)
+	{
+		event.dataTransfer.setData("ids", event.target.id);
+	}
+
+function allowDrop(event)
+	{
+		event.preventDefault();
+	}
+
+function dropped(event)
+	{
+		const dropContainerId = event.target.id;
+		const dropContainerText = event.target.innerText;
+
+		const containerDraggableId = event.dataTransfer.getData("ids");
+		const containerDraggable = document.getElementById(containerDraggableId);
+        const containerDraggableText = containerDraggable.innerText;
+
+		containerDraggable.id = dropContainerId;
+		containerDraggable.innerText = dropContainerText;
+
+		event.target.id = containerDraggableId;
+		event.target.innerText = containerDraggableText;
+	}
